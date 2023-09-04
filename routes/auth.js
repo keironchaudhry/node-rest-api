f650a1adad3e408e027cec231072be38546b958c
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-04 12:40:43 +0200

    Feat: Create logic to post user login

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,6 @@ const bcrypt = require("bcrypt");
 
 // register users
 router.post("/register", async (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
-
   try {
     // generates a hashed password
     const salt = await bcrypt.genSalt(10);
@@ -24,6 +18,24 @@ router.post("/register", async (req, res) => {
 
     // saves created user + returns response
     const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).send("User not found.");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("Wrong password.");
+
     res.status(200).json(user);
   } catch (err) {
     console.log(err);
