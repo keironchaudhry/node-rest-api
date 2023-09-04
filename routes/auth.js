251807ccfc9520dc9b251227c30a448f363ea2fe
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-04 12:18:13 +0200

    Feat: Import bcrypt package to hash user passwords + rearrange code

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 // register users
 router.post("/register", async (req, res) => {
@@ -10,6 +11,18 @@ router.post("/register", async (req, res) => {
   });
 
   try {
+    // generates a hashed password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // creates a new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    // saves created user + returns response
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
