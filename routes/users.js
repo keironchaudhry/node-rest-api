311240643890307aed7c3f8a90aea321cb4abbee
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-05 10:38:00 +0200

    Feat: Create logic to get user parameters + flush out delicate info

--- routes/users.js
+++ routes/users.js
@@ -41,6 +41,16 @@ router.delete("/:id", async (req, res) => {
 });
 
 // Get a user
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
 // Follow a user
 // Unfollow a user
 
