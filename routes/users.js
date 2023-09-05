fb87901d3d3334a92e79682c7b21d147a81aa6bf
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-05 10:28:30 +0200

    Feat: Create logic to delete user

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 
 // Update user
 router.put("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id || req.user.isAdmin) {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
     if (req.body.password) {
       try {
         const salt = await bcrypt.genSalt(10);
@@ -27,6 +27,19 @@ router.put("/:id", async (req, res) => {
 });
 
 // Delete user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted.");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("Incorrect user.");
+  }
+});
+
 // Get a user
 // Follow a user
 // Unfollow a user
