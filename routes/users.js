abcdf50f2c6bcf7d80e881b65b2eceda2560a9e1
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-07 10:31:27 +0200

    Feat: Create logic for unfollowing other user accounts

--- routes/users.js
+++ routes/users.js
@@ -76,5 +76,24 @@ router.put("/:id/follow", async (req, res) => {
 });
 
 // Unfollow a user
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { following: req.params.id } });
+        res.status(200).json("You have unfollowed this user.");
+      } else {
+        res.status(403).json("You do not already follow this user.");
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can only unfollow other user accounts.");
+  }
+});
 
 module.exports = router;
