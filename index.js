e91c29aa94876d3f7afe995ab3d3adb697ecbf80
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-08 10:26:29 +0200

    Feat: Set up posts router

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postsRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -24,6 +25,7 @@ app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postsRoute);
 
 app.listen(port, () => {
   console.log("Backend server is running");
