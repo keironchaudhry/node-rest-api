10d23e0faf6ee85965235f2c5a64511c3206a6bd
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-08 10:55:33 +0200

    Feat: Create logic for creating a post

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,22 @@
 const router = require("express").Router();
+const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-  console.log("Posts");
+// Create a post
+
+router.post("/create", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+// Edit a post
+// Delete a post
+// Like a post
+// View a post
+// View timeline posts
 
 module.exports = router;
