459c8731893d70c1021101b5da51992146364f89
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-09 13:41:04 +0200

    Feat: Create logic to update posts

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const Post = require("../models/Post");
 
 // Create a post
-
 router.post("/create", async (req, res) => {
   const newPost = new Post(req.body);
   try {
@@ -14,6 +13,22 @@ router.post("/create", async (req, res) => {
 });
 
 // Edit a post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({
+        $set: req.body,
+      });
+      res.status(200).json("Post updated.");
+    } else {
+      res.status(403).json("You can only update your own posts.");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Delete a post
 // Like a post
 // View a post
