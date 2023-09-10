8748bb5b114f1e9bdcfabf635c9db0eb23ee38cd
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-10 13:32:42 +0200

    Feat: Add logic to delete user posts

--- routes/posts.js
+++ routes/posts.js
@@ -30,6 +30,20 @@ router.put("/:id", async (req, res) => {
 });
 
 // Delete a post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("Post deleted.");
+    } else {
+      res.status(403).json("You can only delete your own posts.");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Like a post
 // View a post
 // View timeline posts
