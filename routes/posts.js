45ce8a17656fd335946aa373b71030cc3fb7605c
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-14 11:28:33 +0200

    Feat: Filter a feed of posts by userId + followingId

--- routes/posts.js
+++ routes/posts.js
@@ -71,5 +71,19 @@ router.get("/:id", async (req, res) => {
 });
 
 // View timeline posts
+router.get("feed", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.following.map((friendId) => {
+        Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
