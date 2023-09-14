4e2d40c1c76c6e5007eabe02cb61c78969c960b2
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-14 11:13:06 +0200

    Feat: Add logic to view a post

--- routes/posts.js
+++ routes/posts.js
@@ -61,6 +61,15 @@ router.put("/:id/like", async (req, res) => {
 });
 
 // View a post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // View timeline posts
 
 module.exports = router;
