cb4d6720e64d8b68a3252967115c445f2a8b1c14
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-14 10:50:55 +0200

    Feat: Create logic to handle likes + unlikes

--- routes/posts.js
+++ routes/posts.js
@@ -44,7 +44,22 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-// Like a post
+// Like + dislike a post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("You liked this post.");
+    } else {
+      post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("You unliked this post.");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // View a post
 // View timeline posts
 
